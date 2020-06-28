1702ecd47883a2fc0980b68ec3fd5b18a5774c98
faraz-b <f.borghei97@gmail.com>
2020-06-28 14:06:04 -0700

    Added About Dialog Popup

--- dashboard.py
+++ dashboard.py
@@ -5,11 +5,13 @@ from kivy.app import App
 from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.label import Label
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.clock import Clock
 from kivy.graphics import Rectangle, Color
+from kivy.uix.popup import Popup
 
 
 class BatteryTemp(AnchorLayout):
@@ -27,6 +29,21 @@ class VehicleSpeed(AnchorLayout):
 class Faults(AnchorLayout):
     pass
 
+class AboutDialog(BoxLayout):
+    pass
+
+
+class DashBar(AnchorLayout):
+    def show_about_dialog(self):
+        popup = AboutDialog()
+        popup_window = Popup(
+            title='Phantom Dashboard V1.0.0',
+            title_align='center',
+            size_hint= (None, None),
+            size=(400, 400),
+            content=popup)
+        popup_window.open()
+
 
 class Dashboard(AnchorLayout):
     dashSize = ObjectProperty()
@@ -37,9 +54,9 @@ class Dashboard(AnchorLayout):
         #Clock.schedule_interval(self.cb, 1/10)
 
 
-    def on_touch_down(self, touch):
-        print(self.parent.size)
-        print(self.dashSize)
+    # def on_touch_down(self, touch):
+    #     print(self.parent.size)
+    #     print(self.dashSize)
 
     def cb(self, *largs):
         print('Hello, World')
